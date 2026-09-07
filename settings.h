333de87ae6c0ae457cc3ebd97f6aac0ca5548964
Rich5252 <richard.lax7@gmail.com>
2026-09-07 09:55:11 +0100

    Tweak Preset2/3

--- settings.h
+++ settings.h
@@ -248,7 +248,7 @@ AUDIO_SRC_TWOTONE, 0.00f, 0.20f, 0.90f, false, ADC_LPF_MODE_OFF, false, false, -
 SSB_DSP_FREQ_DEV_SLEW_UNLIMITED_HZ, false, ENVELOPE_INTERP_CURVE_CATMULL_ROM },
 
 // Preset 2 - Single-tone test
- { "Shelf2 Baseline pre grp adj#3", AUDIO_SRC_TWOTONE, 2.00f, 0.20f, 0.90f, true, ADC_LPF_MODE_OFF, false, false, -1.4f, true, true, 0.00f, SSB_DSP_FREQ_DEV_SLEW_UNLIMITED_HZ, false, ENVELOPE_INTERP_CURVE_CATMULL_ROM, true, true, ENV_GDEQ_VARIANT_DEFAULT },
+ { "Shelf2 Baseline pre grp adj#2", AUDIO_SRC_TWOTONE, 2.03f, 0.20f, 0.90f, true, ADC_LPF_MODE_OFF, false, false, -1.4f, true, true, 0.00f, SSB_DSP_FREQ_DEV_SLEW_UNLIMITED_HZ, false, ENVELOPE_INTERP_CURVE_CATMULL_ROM, true, true, ENV_GDEQ_VARIANT_DEFAULT },
 
  // Preset 3 - Envelope / PWM test
  { "Shelf2 Baseline pre grp adj#4", AUDIO_SRC_TWOTONE, 2.00f, 0.20f, 0.90f, true, ADC_LPF_MODE_OFF, false, false, -1.4f, true, true, 0.00f, SSB_DSP_FREQ_DEV_SLEW_UNLIMITED_HZ, false, ENVELOPE_INTERP_CURVE_CATMULL_ROM, true, true,  ENV_GDEQ_VARIANT_CANDIDATE_B },
